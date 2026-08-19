418c55da014ef47b92ef3fbae75e2e8c76729115
Sunken3410 <kareemnajar5@gmail.com>
2026-08-19 18:58:08 +0300

    Revert "initial commit supporting auth"
    This reverts commit 7c0f0cc43b5b8674c59b7168cc2cd949b168bf0c.

--- app/doctor/layout.tsx
+++
@@ -1,6 +0,0 @@
-import { RoleGuard } from "@/components/auth/RoleGuard";
-import type { ReactNode } from "react";
-
-export default function DoctorLayout({ children }: { children: ReactNode }) {
-  return <RoleGuard allowedRoles={["Doctor"]}>{children}</RoleGuard>;
-}
--- app/patient/layout.tsx
+++
@@ -1,6 +0,0 @@
-import { RoleGuard } from "@/components/auth/RoleGuard";
-import type { ReactNode } from "react";
-
-export default function PatientLayout({ children }: { children: ReactNode }) {
-  return <RoleGuard allowedRoles={["Patient"]}>{children}</RoleGuard>;
-}
